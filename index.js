da1cd06272e144f618bf197530c12a3b893b4c4c
Maggiekivaa <kivaamaggie@gmail.com>
2024-11-26 13:20:00 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = 'Maggie Kivaa';
+const welcomeMessage = "Welcome to Flatbook, Maggie Kivaa " + '!';
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, MAGGIE KIVAA " + '!';
+const shortGreeting =" Welcome, M!";
+
+
+
+
